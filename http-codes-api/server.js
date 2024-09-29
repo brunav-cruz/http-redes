efcd6b446f6644b5699b4c7084ac58f660f06e3b
Bruna Cruzl <brunavascruz@gmail.com>
2024-09-29 16:57:07 -0300

    feat:  ajuste na api

---
+++ http-codes-api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors'); 
+const app = express();
+const port = process.env.PORT || 3122;
+const httpCodesRoutes = require('./routes/http');
+const path = require('path');
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', httpCodesRoutes);
+
+app.listen(port, () => {
+  console.log(`API rodando na porta: ${port}...`);
+});
